4640536e51fbcacd895fb057f2d61baf3ed23e5b
Michelle <michellestefanie@hotmail.com>
2019-04-09 17:25:52 +0200

    Eindoplevering

--- opdracht3/filteren.js
+++ opdracht3/filteren.js
@@ -1,4 +1,5 @@
 var body = document.querySelector('body');
+var alleGenresKnop = document.querySelector('.genres');
 var actionKnop = document.querySelector('.action');
 var adventureKnop = document.querySelector('.adventure');
 var crimeKnop = document.querySelector('.crime');
@@ -6,29 +7,38 @@ var dramaKnop = document.querySelector('.drama');
 var horrorKnop = document.querySelector('.horror');
 var thrillerKnop = document.querySelector('.thriller');
 
+alleGenresKnop.addEventListener('click', function(){
+    body.classList = '';
+});
+
 
 actionKnop.addEventListener('click', function(){
-    body.classList.remove
+    body.classList = '';
   body.classList.toggle('action');
 });
 
 adventureKnop.addEventListener('click', function(){
+    body.classList = '';
   body.classList.toggle('adventure');
 });
 
 crimeKnop.addEventListener('click', function(){
+    body.classList = '';
   body.classList.toggle('crime');
 });
 
 dramaKnop.addEventListener('click', function(){
+    body.classList = '';
   body.classList.toggle('drama');
 });
 
 horrorKnop.addEventListener('click', function(){
+    body.classList = '';
   body.classList.toggle('horror');
 });
 
 thrillerKnop.addEventListener('click', function(){
+    body.classList = '';
   body.classList.toggle('thriller');
 });
 
